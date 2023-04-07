0db984ce7c3b78da46baa3ffb8376e476d9a4216
Vinicius Koji Enari <viniciusenari@gmail.com>
2023-04-07 14:08:40 -0300

    feat: add imports to models init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,3 @@
 from app.models.user_models import User, UserInDB, UserBase, UserBaseInDB  # noqa: F401
+from app.models.shelter_models import Shelter, ShelterBase  # noqa: F401
+from app.models.pet_models import Pet, PetBase  # noqa: F401
